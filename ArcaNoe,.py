bbaef3545483dfcfc92b3dbbe637658763dbda33
joanizba <82592525+joanizba@users.noreply.github.com>
2024-10-29 19:21:04 +0100

    Add files via upload

---
+++ ArcaNoe,.py
@@ -0,0 +1,27 @@
+class Arca:
+    animales = []
+    alimentos = []
+    agua = 0
+    capacidad_maxima = capacidad_maxima
+
+    @staticmetohd
+    def __init__(cls, capacidad_maxima):
+        cls.capacidad_maxima = capacidad_maxima
+    
+
+    def agreagar_animal(cls, animal):
+        return cls.animales.append(animal)
+
+    
+    def agrar_alimento(cls, alimentos):
+        return cls.alimentos.append(alimentos)
+    
+    def alimentar_animal()
+        return  0
+    
+    
+    def dar_agua():
+        return agua
+
+
+    def estado_arca():
