56f52799ff2eef979b29264c52e1daed39d061e2
Armen Hakobyan <ahakobyan@seidor.es>
2024-11-05 19:34:44 +0100

    añadiendo modificaciones iniciales

--- alimento.py
+++ alimento.py
@@ -1,12 +1,7 @@
 class Alimento:
-
     def __init__(self, tipo, cantidad):
         self.tipo = tipo
         self.cantidad = cantidad
-
-    def usar(self, cantidad):
-        return cantidad
-    
-    @staticmethod
-    def es_alimento_adecuado(cls, tipo_animal):
-        return True
+        
+    def usar(self, cantidad_u):
+        self.cantidad -= cantidad_u
